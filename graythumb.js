957b86c07db34a42160a35a5c1d8ae6d64cd8918
Diego Sandoval <sandovdiego@gmail.com>
2021-06-01 03:37:05 -0400

    formatting

--- graythumb.js
+++ graythumb.js
@@ -9,9 +9,11 @@ const declaration = "{filter: grayscale(100%);}";
 
 const rule = selectors.join(', ') + declaration;
 
-if(style.styleSheet){
-    style.styleSheet.cssText=rule;
-}else{
+if(style.styleSheet) {
+    style.styleSheet.cssText = rule;
+}
+else {
     style.appendChild(document.createTextNode(rule));
 }
+
 document.getElementsByTagName('head')[0].appendChild(style);
