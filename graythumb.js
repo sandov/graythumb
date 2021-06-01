94d612ad8b488e9bda43517d6bda4a46cd0720ea
Diego Sandoval <sandovdiego@gmail.com>
2021-06-01 01:18:23 -0400

    rm jquery

--- graythumb.js
+++ graythumb.js
@@ -1,28 +1,27 @@
-/* Import jQuery */
-var script = document.createElement('script');
-script.src = './vendor/jquery/jquery-3.6.0.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
-/* End of import */
+homepage_grid_selector = "#contents.style-scope.ytd-rich-grid-renderer";
+homepage_grid = document.querySelector(homepage_grid_selector);
 
-let apply_to_avatars = true;
-let apply_to_thumbnails = true;
-let apply_to_previews = false;
-
-document.onload = apply();
-
-function apply() {
-
-    let thumbnails_selector = ".ytd-thumbnail img";
-    let avatars_selector = ".avatar img";
-
-    if (apply_to_thumbnails){
-        thumbnails = document.querySelectorAll(thumbnails_selector);
-        thumbnails.forEach(thumbnail => thumbnail.style.filter = "grayscale(100%)");
-    }
-    
-    /*if (apply_to_avatars){
-        avatars = document.querySelectorAll(avatars_selector);
-        avatars.forEach(avatar => avatar.style.filter = "grayscale(100%)");
-    }*/
+const callback = function(mutList, observer){
+    mutList.forEach(mut => {
+        console.log("mutation");
+    })
 }
+
+let observer = new MutationObserver(callback);
+const observer_config = {childList: true};
+observer.observe(homepage_grid, observer_config);
+
+/*
+let thumbnails_selector = ".ytd-thumbnail img";
+let avatars_selector = ".avatar img";
+
+if (apply_to_thumbnails){
+    thumbnails = document.querySelectorAll(thumbnails_selector);
+    thumbnails.forEach(thumbnail => thumbnail.style.filter = "grayscale(100%)");
+}
+
+if (apply_to_avatars){
+    avatars = document.querySelectorAll(avatars_selector);
+    avatars.forEach(avatar => avatar.style.filter = "grayscale(100%)");
+}
+*/
