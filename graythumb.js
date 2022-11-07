41b0d023982798b9f77e250842a65a80eafa5d27
Diego Sandoval <sandovdiego@gmail.com>
2022-11-06 22:50:59 -0300

    add playlist thumbnail class to selector

--- graythumb.js
+++ graythumb.js
@@ -3,6 +3,7 @@ let style = document.createElement('style');
 const selectors = [
     'ytd-thumbnail',
     '#avatar img',
+	'ytd-playlist-thumbnail'
 ];
 
 const declaration = "{filter: grayscale(100%);}";
